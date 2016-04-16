9e7e851f7030b4bfbf33472e315f9e44cf4f7594
Julia Dohner <jdohner@cs.hmc.edu>
2016-04-15 22:10:40 -0700

    folder added

--- README.md
+++
@@ -1,1 +0,0 @@
-# hackathon-spring-2016
---
+++ projectFolder/pythonWebStuff.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+import json
+from flask import render_template, request
+
+@app.route("/")
+def webDoc():
+    # return "Hello!"
+    return render_template("webDoc.html")
+
+if __name__ == "__main__":
+    app.run()
