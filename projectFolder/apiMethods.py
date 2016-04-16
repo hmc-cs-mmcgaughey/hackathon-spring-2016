e3e08db20f437bb3f95565240080fa0e80d3c0b8
Faiz Abdulali <fabdulali@cs.hmc.edu>
2016-04-16 04:26:52 -0700

    ??

--- projectFolder/apiMethods.py
+++ projectFolder/apiMethods.py
@@ -1,5 +1,5 @@
-import requests, json
-api_key = "U6qwIj4pbaXQRdwUejhQGJdR4jh4HjBQqYMaIZac"
+import requests, json, csv
+api_key = "8nmhccHjthYxNPcUkdoiVYUfeud2hbYCsrbrjllf"
 
 def getWaterQuantity(foodName, quantity, unit):
     """inputs: foodName (string) - name of food
@@ -113,4 +113,70 @@ def showInOtherQuantity(waterInGallons, other):
     num = dictionaryToReturn[other]
     return waterInGallons/num
 
-6
+def read_csv_file( csv_file_name ):
+    """
+        returns a Python list of all of the data
+        doesn't include the header row
+        doesn't convert from strings
+          == this is a _starting_ point for further data pre-processing
+    """
+    # is the file here?
+    try:
+                  
+        csvfile = open( csv_file_name, newline='' )  
+        csvrows = csv.reader( csvfile )              # creates a csvrows object
+    except FileNotFoundError as e:
+        print("File not found: ", e)
+
+
+    List_of_rows = []                  # into a Python list, not yet a numpy array
+    
+    for row in csvrows:                # into our own Python data structure
+        # print("row is", row)            # debugging only!
+            
+        # convert row to strings
+        row = list([ str(x) for x in row])
+        List_of_rows.append( row )        # add the current row _as an element_
+    
+    return List_of_rows
+def showInOtherQuantity(waterInGallons, other):
+    
+    dictionaryToReturn = {'Showers':3.5, 'Toilet':1.6, 'Laundry':waterInGallons/30.0, 'Dishes':waterInGallons/10.0}
+    
+    num = dictionaryToReturn[other]
+    return num
+
+def isSimilar(new_content, original_content):
+    if abs((new_content/100.0)-(original_content/100.0))<0.0001:
+        return True
+    else:
+        return False
+        
+def compare_foods_with_similar(foodName, nutrient):
+    
+    list_of_rows = read_csv_file("ABBREV.csv")
+    
+    #find row number of nutrient
+    counter = 0
+    for title in list_of_rows[0]:
+        if nutrient in title:
+            break
+        counter += 1
+        
+    original_nutrient_content = float(getNutrientContentOfFood(foodName,nutrient))
+    similar_foods = []
+    for i in range(1,len(list_of_rows)):
+        name = list_of_rows[i][1]
+        protein_content = list_of_rows[i][counter]
+        if isSimilar(float(protein_content),original_nutrient_content):
+            similar_foods += [name]
+    
+    try:
+        return similar_foods[0:3]
+    except:
+        return similar_foods[0]
+        
+    
+    
+    
+print(compare_foods_with_similar('beef','Energy'))
